3a7c4cee76c37eaf97831003563fa562ec120610
Rodrigo Dominguez <rodrigo.dominguez@kavak.com>
2022-09-20 14:14:40 -0300

    verticalArrangement demos

--- app/src/main/java/com/rodrigodominguez/dynamicislandwithmotionlayoutcompose/MainActivity.kt
+++ app/src/main/java/com/rodrigodominguez/dynamicislandwithmotionlayoutcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rodrigodominguez.dynamicislandwithmotionlayoutcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Alignment
@@ -15,7 +16,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
                 DynamicIsland()
                 DynamicIslandDemo2()
                 DynamicIslandDemo3()
